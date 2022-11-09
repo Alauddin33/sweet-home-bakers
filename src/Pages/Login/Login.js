d599fa7437f70ff030a3e1b26aa200627555adce
Alauddin33 <shamem33@gmail.com>
2022-11-09 15:17:17 +0600

    register form added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -4,6 +4,11 @@ import { FaGoogle } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const handleLogin = event => {
+        event.preventDefault();
+    }
+
     return (
         <div data-theme="retro">
             <div className="hero min-h-screen bg-base-200">
@@ -13,31 +18,31 @@ const Login = () => {
                         <img width={500} height={500} src={img} alt="" />
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100  py-1">
-                        <div className="card-body">
+                        <form onSubmit={handleLogin} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
+                                <input type="email" placeholder="email" name='email' required className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <input type="password" placeholder="password" name='password' required className="input input-bordered" />
                                 <label className="label flex flex-col">
-                                    <Link className="label-text-alt link link-hover">Forgot password?</Link>
-                                    <Link className="label-text-alt link link-hover  py-5">New to this site? Please, Register now</Link>
+                                    <Link className="label-text-alt link link-hover text-orange-800">Forgot password?</Link>
                                 </label>
                             </div>
                             <div className="form-control my-4">
-                                <button className="btn btn-info">Login</button>
+                                <input className="btn btn-info" type="submit" value="Login" />
                             </div>
-                            <div className=' text-center '>
-                                <p>Login with Google</p>
-                                <button type="submit"><FaGoogle></FaGoogle></button>
-                            </div>
+                        </form>
+                        <div className='border-2 border-indigo-600 rounded-md w-3/4 mx-auto text-center'>
+                            <p>Login with Google</p>
+                            <button><FaGoogle></FaGoogle></button>
                         </div>
+                        <p className=" text-center text-xs py-5">New to this site? Please, <Link to='/register' className=' text-orange-800 link'> Register now</Link></p>
                     </div>
                 </div>
             </div>
---
+++ src/Pages/Register/Register.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import img from '../../assets/login.jpg'
+
+const Register = () => {
+
+    const handleRegister = event => {
+        event.preventDefault();
+    }
+
+    return (
+        <div data-theme="retro">
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold text-center pb-4">Register now!</h1>
+                        <img width={500} height={500} src={img} alt="" />
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 ">
+                        <form onSubmit={handleRegister} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="Your Name" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' placeholder="email" required className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" required className="input input-bordered" />
+                            </div>
+                            <div className="form-control my-4">
+                                <input className="btn btn-info" type="submit" value="Register" />
+                            </div>
+                        </form>
+                        <p className=" text-center text-xs pb-5">Already have an account? Please, <Link to='/login' className=' text-orange-800 link'> Login</Link></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
